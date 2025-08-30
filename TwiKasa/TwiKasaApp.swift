00cde1373597dbbf751b42ff573fa249842b6e0a
Denis Ansah <Denis.Ansah@yahoo.com>
2025-08-30 00:12:42 -0400

    Initial Commit

---
+++ TwiKasa/TwiKasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwiKasaApp.swift
+//  TwiKasa
+//
+//  Created by Throw Catchers on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TwiKasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
